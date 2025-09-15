119fa8e2976a44873445ae83d290a1495db3394f
rt-heroku <rodrigo.torres@mulesoft.com>
2025-09-15 19:33:55 -0400

    Fix syntax error by rewriting batch rendering section with cleaner structure and add local syntax checking

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'no-undef': 'error'
+  },
+  globals: {
+    React: 'readonly',
+    window: 'readonly',
+    document: 'readonly',
+    console: 'readonly'
+  }
+};
